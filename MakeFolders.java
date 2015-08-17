50e014f8d376bb9bf479610524623d4b9988ecaf
Matthew Boyette <Dyndrilliac@gmail.com>
2015-08-17 18:25:48 -0400

    Updated to reflect API changes.

--- MakeFolders.java
+++ MakeFolders.java
@@ -2,7 +2,7 @@
  * Title: MakeFolders
  * Author: Matthew Boyette
  * Date: 11/12/2012
- *
+ * 
  * This is a simple program to take plain-text files with folder names on each line and then automatically generate the folders.
  */
 
@@ -17,13 +17,13 @@ public class MakeFolders
 	{
 		new MakeFolders(args);
 	}
-
+	
 	private boolean	isDebugging	= false;
-
+	
 	public MakeFolders(final String[] args)
 	{
 		this.setDebugging(Support.promptDebugMode(null));
-
+		
 		if (args.length > 0)
 		{
 			for (String arg: args)
@@ -36,40 +36,40 @@ public class MakeFolders
 			this.readFile("");
 		}
 	}
-
+	
 	public final boolean isDebugging()
 	{
 		return this.isDebugging;
 	}
-
+	
 	protected final void makeFolder(final String folderName)
 	{
 		File folder = new File(folderName);
 		folder.mkdir();
 	}
-
+	
 	protected final void readFile(final String filePath)
 	{
 		String newFilePath = new String(filePath.toCharArray());
-
+		
 		if ((newFilePath == null) || newFilePath.isEmpty())
 		{
 			newFilePath = Support.getFilePath(null, true, this.isDebugging());
 		}
-
+		
 		if ((newFilePath == null) || newFilePath.isEmpty())
 		{
 			// Either the user has canceled the file operation or something has gone wrong; abort!
 			return;
 		}
-
+		
 		Scanner inputStream = null; // Stream object for file input.
-
+		
 		try
 		{
 			// Initialize file stream. If the given path is invalid, an exception is thrown.
 			inputStream = new Scanner(new File(newFilePath));
-
+			
 			while (inputStream.hasNextLine())
 			{
 				// Get the next line in the original file.
@@ -93,7 +93,7 @@ public class MakeFolders
 			}
 		}
 	}
-
+	
 	public final void setDebugging(final boolean isDebugging)
 	{
 		this.isDebugging = isDebugging;
